d434f53cc7b9d03518aac964017bbcfb6b3b0822
Sai kumaresh <43513133+saikumaresh@users.noreply.github.com>
2024-10-11 21:54:27 +0530

    Update FibonacciSeries.py

--- Algorithms/Recursion_and_Backtracking/FibonacciSeries.py
+++ Algorithms/Recursion_and_Backtracking/FibonacciSeries.py
@@ -1,9 +1,9 @@
 def fibonacci(n):
-    if(n == 0):
+    if(n == 0): # Base Condition 1
        return 0
-    if (n == 1): 
+    if (n == 1): # Base Condition 2
         return 1
-    return fibonacci(n-1) + fibonacci(n-2)
+    return fibonacci(n-1) + fibonacci(n-2) # Main Logic
 
 number = 10
 for i in range(1, number):

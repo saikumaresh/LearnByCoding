8686b34eed503996217609e848397bb20b44df29
Sai kumaresh <43513133+saikumaresh@users.noreply.github.com>
2024-10-16 13:07:32 +0530

    Update Factorial.py

--- Algorithms/Recursion_and_Backtracking/Factorial.py
+++ Algorithms/Recursion_and_Backtracking/Factorial.py
@@ -6,3 +6,17 @@ def factorial(number):
 
 print(factorial(2))
 print(factorial(5))
+
+#########################################
+
+class Solution:
+    # @param A : integer
+    # @return an integer
+    def solve(self, A):
+        if A==1:
+            return 1
+        return self.solve(A-1)*A
+    
+sol = Solution()
+print(sol.solve(2))
+print(sol.solve(5))

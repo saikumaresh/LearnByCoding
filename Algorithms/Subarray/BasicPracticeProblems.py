f01e1703225d2b2757a35ff1b3e6bfcf4edcf4bc
Sai kumaresh <43513133+saikumaresh@users.noreply.github.com>
2024-10-24 02:47:36 +0530

    Update BasicPracticeProblems.py

--- Algorithms/Subarray/BasicPracticeProblems.py
+++ Algorithms/Subarray/BasicPracticeProblems.py
@@ -38,7 +38,8 @@ print_all_subarrays(nums)
 
 
 ## 3. Subarray Product Less Than K
-# Problem: Given an array of integers `nums` and an integer `k`, return the number of contiguous subarrays where the product of all elements in the subarray is strictly less than `k`.
+# Problem: Given an array of integers `nums` and an integer `k`, return the number of contiguous 
+# subarrays where the product of all elements in the subarray is strictly less than `k`.
 # Input: nums = [10, 5, 2, 6], k = 100
 # Output: 8
 # Explanation: The subarrays are [10], [5], [2], [6], [10,5], [5,2], [2,6], [5,2,6].
